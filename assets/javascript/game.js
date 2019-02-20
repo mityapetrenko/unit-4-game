d29d13d085cc78d60af343c08e687e4aaf6b2c40
mityapetrenko <dimbaslv@gmail.com>
2019-02-19 20:15:17 -0600

    display score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,39 @@
-var numberOptions = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+$( document ).ready( function(){
+//Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var numberOptions = [1,2,3,4,5,6,7,8,9,10,11,12]
 var wins=0;
 var losses=0;
-var imageCrystal=$('src','.crystal-images');
 var targetNumber=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 var counter=0;
-console.log(imageCrystal);
-console.log(numberOptions);
+
 $("#computerGuess").text(targetNumber);
-
 $("#userScore").text(counter);
-
-imageCrystal.attr("data-crystalvalue", numberOptions);
+$("#winlose").text("Wins " + wins);
+$("#winlose").text("Losses " + losses);
+for (var i = 0; i < numberOptions.length; i++) {
+    var imageCrystal=$("<img>");
+    imageCrystal.addClass("crystal-image");
+imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+console.log(imageCrystal);
+}
 
 $(".crystal-images").on("click", function() {
-
+    
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
-    console.log(crystalValue);
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
-
+    console.log(counter);
+    console.log(crystalValue)
     if (counter === targetNumber) {
-      $("#userScore").append(counter);
+      
+      wins++; counter=0; score=0;
     }
 
     else if (counter >= targetNumber) {
-      alert("You lose!!");
+    
+      losses--; counter=0;
     }
-console.log(counter);
-  });
+  
 
+  });
+});
